e5ca5eaf46d36885a48571105636e5c7c909cceb
Gabriel Henrique <gabrielhenrique@byronsolutions.com>
2022-06-18 11:43:58 -0300

    Alterações Finais Formulario, Ajuste no HEAD e Otimização do Css

---
+++ enviar-sendgrid.php
@@ -0,0 +1,36 @@
+<?php
+
+require("./sendgrid-php/sendgrid-php.php");
+
+$email_site = "gabrielhenrique@byronsolutions.com";
+$nome_site = "Bikcraft";
+
+$email_user = $_POST["email"];
+$nome_user = $_POST["nome"];
+
+$body_content = "";
+foreach( $_POST as $field => $value) {
+  if( $field !== "leaveblank" && $field !== "dontchange" && $field !== "enviar") {
+    $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+    $body_content .= "$field: $value \n";
+  }
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject("Formulário Bikcraft");
+$email->addContent("text/plain", $body_content);
+
+$sendgrid = new \SendGrid("SG.2kM8Ik47RYWwzxqwgLi20w.cP2T9X30V_AXAG8kIwQN3F04Loi8yeOAN95drMO4WkYs");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo "Caught exception: ". $e->getMessage() ."\n";
+}
